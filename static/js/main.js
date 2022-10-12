644f21f231c4f42bc61eadb576b5dbd310884737
Fritz Dischner <fritz@dischner.me>
2022-10-11 19:56:02 -0700

    Update header and footer

--- static/js/main.js
+++ static/js/main.js
@@ -1,18 +1,19 @@
 let scrollpos = window.scrollY;
-const header = document.querySelector(".main-nav");
-const logo = document.querySelector(".main-nav__logo");
+const header = document.querySelector("header");
+const nav = document.querySelector(".main-nav");
+const mainNavLeft = document.querySelector(".main-nav__left");
 const header_height = header.offsetHeight;
 
 window.addEventListener("scroll", () => {
   scrollpos = window.scrollY;
 
   if (scrollpos >= header_height) {
-    header.classList.add("scrolled");
-    logo.classList.add("visible");
+    nav.classList.add("scrolled");
+    mainNavLeft.classList.add("visible");
     return;
   }
-  header.classList.remove("scrolled");
-  logo.classList.remove("visible");
+  nav.classList.remove("scrolled");
+  mainNavLeft.classList.remove("visible");
 });
 
 // window.addEventListener("click", () => {
