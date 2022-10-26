f08d31fff1492bf561738cc238530f9a7832a083
Fritz Dischner <fritz@dischner.me>
2022-10-25 20:48:57 -0700

    Update footer
    Added additional site links to the footer

--- static/js/modal.js
+++ static/js/modal.js
@@ -17,7 +17,7 @@ var info = {
 	},
 	map: {
 		title: "<span style='display:none;'></span>",
-		info: "<iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3253.2732164171744!2d-119.02267768380183!3d35.373679080266996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80ea695b002b694f%3A0x5abafbf3c722a1a6!2sBakersfield%20City%20Hall!5e0!3m2!1sen!2sus!4v1666566060861!5m2!1sen!2sus' width='100%' height='900' style='border:0;' allowfullscreen='' loading='lazy' referrerpolicy='no-referrer-when-downgrade'></iframe>",
+		info: "<iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3253.2732164171744!2d-119.02267768380183!3d35.373679080266996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80ea695b002b694f%3A0x5abafbf3c722a1a6!2sBakersfield%20City%20Hall!5e0!3m2!1sen!2sus!4v1666566060861!5m2!1sen!2sus' style='border:0;' allowfullscreen='' loading='lazy' referrerpolicy='no-referrer-when-downgrade'></iframe>",
 	},
 };
 
