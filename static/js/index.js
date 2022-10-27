8aaaf82d658d4730a3a1ed2320cf8517b2ca85f3
Fritz Dischner <fritz@dischner.me>
2022-10-27 11:52:13 -0700

    Fix video/audio in index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,15 +1,30 @@
 const lol = document.getElementById("lol");
 const plz = document.getElementById("plz-dont");
+const sad = document.getElementById("sad-face");
+const jams = new Audio("https://www.dischner.me/sounds/kiss_from_a_rose.mp3");
 
-genLolz = () => {
-	const lolz = new Audio("https://www.dischner.me/sounds/kiss_from_a_rose.mp3");
-	lolz.play();
+genjams = () => {
+	jams.play();
 	let vid = document.createElement("video");
 	vid.src = "https://www.dischner.me/video/lol.mp4";
 	vid.autoplay = true;
 	vid.style =
-		"position: absolute; top: 0; left: 0; width: 100vw; height: 100vh; z-index: 100;";
+		"position: absolute; top: 0; left: 0; width: 100vw; height: 100vh; z-index: 100; object-fit: cover;";
+	vid.addEventListener("ended", () => {
+		sad.classList.toggle("hide");
+		plz.classList.toggle("hide");
+		lol.removeChild(vid);
+	});
 	lol.appendChild(vid);
+
 };
 
-plz.addEventListener("click", genLolz);
+genSad = () => {
+	jams.pause();
+	jams.currentTime = 0;
+	sad.classList.toggle("hide");
+	plz.classList.toggle("hide");
+};
+
+plz.addEventListener("click", genjams);
+sad.addEventListener("click", genSad);
--- static/js/main.js
+++ static/js/main.js
@@ -5,13 +5,13 @@ const mainNavLeft = document.querySelector(".main-nav__left");
 const header_height = header.offsetHeight;
 
 window.addEventListener("scroll", () => {
-  scrollpos = window.scrollY;
+	scrollpos = window.scrollY;
 
-  if (scrollpos >= header_height) {
-    nav.classList.add("scrolled");
-    mainNavLeft.classList.add("visible");
-    return;
-  }
-  nav.classList.remove("scrolled");
-  mainNavLeft.classList.remove("visible");
+	if (scrollpos >= header_height) {
+		nav.classList.add("scrolled");
+		mainNavLeft.classList.add("visible");
+		return;
+	}
+	nav.classList.remove("scrolled");
+	mainNavLeft.classList.remove("visible");
 });
